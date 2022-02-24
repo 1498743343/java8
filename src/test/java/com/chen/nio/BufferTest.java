49b9a4a725b59559e22cb3f1eb97a53bb2635d50
chenzihan <chenzihan@thunisoft.com>
2022-02-24 11:07:51 +0800

    feat: 完善 buffer 代码

--- src/test/java/com/chen/nio/BufferTest.java
+++ src/test/java/com/chen/nio/BufferTest.java
@@ -66,14 +66,14 @@ public class BufferTest {
         IntStream.rangeClosed(1, 5)
                 .forEach(intBuffer::put);
         intBuffer.flip();
-        IntStream.rangeClosed(1,2)
+        IntStream.rangeClosed(1, 2)
                 .forEach(i -> {
                     // 指定 index 读取不会改变 position 的值
                     int result = intBuffer.get(i);
                     log.info("result = {}", result);
                     log.info("position = {}", intBuffer.position());
                 });
-        IntStream.rangeClosed(1,4)
+        IntStream.rangeClosed(1, 4)
                 .forEach(i -> {
                     // 不指定 index 的值，会改变 position 的值
                     int result = intBuffer.get();
@@ -86,7 +86,7 @@ public class BufferTest {
     }
 
     /**
-     * 倒带：读取完以后，调用 rewind 方法可以重新读取
+     * 倒带：读取完以后，调用 rewind 方法可以重置 position 和 mark 的状态，从头开始读取
      */
     @Test
     public void rewind() {
@@ -94,11 +94,73 @@ public class BufferTest {
         IntStream.rangeClosed(1, 5)
                 .forEach(intBuffer::put);
         intBuffer.flip();
-        IntStream.rangeClosed(1,2)
+        IntStream.rangeClosed(1, 2)
                 .forEach(i -> intBuffer.get());
         intBuffer.rewind();
         log.info("position = {}", intBuffer.position());
         log.info("limit = {}", intBuffer.limit());
         log.info("capacity = {}", intBuffer.capacity());
+    }
+
+    /**
+     * mark & reset，这两个方法一般配套使用，mark 记录 position 某一刻的状态，后续改变后，可以调用 reset 方法还原到 mark 时的状态
+     */
+    @Test
+    public void reset() {
+        IntBuffer intBuffer = IntBuffer.allocate(10);
+        IntStream.rangeClosed(1, 5)
+                .forEach(intBuffer::put);
+        intBuffer.flip();
+        IntStream.rangeClosed(0, 4)
+                .forEach(i -> {
+                    if (i == 2) {
+                        intBuffer.mark();
+                    }
+                    int result = intBuffer.get();
+                    log.info("result = {}", result);
+                });
+        log.info("position = {}", intBuffer.position());
+        log.info("limit = {}", intBuffer.limit());
+        log.info("capacity = {}", intBuffer.capacity());
+        intBuffer.reset();
+        log.info("position = {}", intBuffer.position());
+        log.info("limit = {}", intBuffer.limit());
+        log.info("capacity = {}", intBuffer.capacity());
+    }
+
+    /**
+     * 清空缓冲区：会将 buffer 重置到初始的写状态，即 position = 0;limit = capacity;mark = -1;
+     * 但是 clear 不会清空 buffer 的数组，所以通过 get(index) 还是可以获取固定位置下的元素
+     */
+    @Test
+    public void clear() {
+        IntBuffer intBuffer = IntBuffer.allocate(10);
+        IntStream.rangeClosed(1, 5)
+                .forEach(intBuffer::put);
+        intBuffer.flip();
+        intBuffer.clear();
+        intBuffer.compact();
+        IntStream.rangeClosed(0, 4)
+                .forEach(i -> {
+                    int result = intBuffer.get(i);
+                    log.info("result = {}", result);
+                });
+    }
+
+    /**
+     * 压缩 buffer，具体规则参见 IntBuffer.compact 的注释
+     */
+    @Test
+    public void compact() {
+        IntBuffer intBuffer = IntBuffer.allocate(10);
+        IntStream.rangeClosed(0, 4)
+                .forEach(intBuffer::put);
+        intBuffer.flip();
+        intBuffer.compact();
+        IntStream.rangeClosed(0, 9)
+                .forEach(i -> {
+                    int result = intBuffer.get(i);
+                    log.info("result = {}", result);
+                });
     }
 }
