917f981c728ebc762902cfdfe934cd926da1ce33
chenzihan <chenzihan@thunisoft.com>
2022-09-21 14:10:05 +0800

    feat: 新增 main

--- src/test/java/com/chen/Test.java
+++ src/test/java/com/chen/Test.java
@@ -9,6 +9,10 @@ public class Test {
         System.out.println("test main");
     }
 
+    public static void main(String[] args) {
+        System.out.println("test main");
+    }
+
     public void test2() {
         System.out.println("this is test2");
     }
