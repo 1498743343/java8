36781a7529054dd9cf13276613e787916fe22b90
chenzihan <chenzihan@thunisoft.com>
2022-09-21 14:16:29 +0800

    feat: 重置

--- src/test/java/com/chen/Test.java
+++ src/test/java/com/chen/Test.java
@@ -1,19 +1,7 @@
 package com.chen;
 
 public class Test {
-    public void test1() {
-        System.out.println("this is test1");
-    }
-
     public static void main(String[] args) {
-        System.out.println("test main");
-    }
-
-    public static void main(String[] args) {
-        System.out.println("test main");
-    }
-
-    public void test2() {
-        System.out.println("this is test2");
+        System.out.println("this is main");
     }
 }
