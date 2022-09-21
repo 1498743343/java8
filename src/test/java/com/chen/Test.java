7a61bfbaa91d3956a324eba4557f600f8f30e4a9
chenzihan <chenzihan@thunisoft.com>
2022-09-21 14:00:28 +0800

    feat: 新增test2

--- src/test/java/com/chen/Test.java
+++ src/test/java/com/chen/Test.java
@@ -8,4 +8,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test main");
     }
+
+    public void test2() {
+        System.out.println("this is test2");
+    }
 }
