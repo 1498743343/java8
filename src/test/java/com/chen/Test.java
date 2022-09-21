48b92221dafe8204e1d54d8cf1244dc2ab2fceca
chenzihan <chenzihan@thunisoft.com>
2022-09-21 14:17:30 +0800

    feat: test1 新增方法

--- src/test/java/com/chen/Test.java
+++ src/test/java/com/chen/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("this is main");
     }
+
+    public void t1() {
+        System.out.println("this is test1");
+    }
 }
