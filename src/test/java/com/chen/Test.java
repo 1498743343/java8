bf012b6aa2671ac64b4d3f1c8d56e882e84c3d5b
chenzihan <chenzihan@thunisoft.com>
2022-09-21 14:19:23 +0800

    feat: main 新增方法2

--- src/test/java/com/chen/Test.java
+++ src/test/java/com/chen/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("this is main");
     }
+
+    public void main() {
+        System.out.println("this is main 2");
+    }
 }
