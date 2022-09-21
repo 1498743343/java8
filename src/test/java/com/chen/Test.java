d9ebde253ecc1c573b0e6588aa16148284fab32a
chenzihan <chenzihan@thunisoft.com>
2022-09-21 14:18:08 +0800

    feat: test1 改变 main

--- src/test/java/com/chen/Test.java
+++ src/test/java/com/chen/Test.java
@@ -2,7 +2,7 @@ package com.chen;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("this is main");
+        System.out.println("this is t1 change main");
     }
 
     public void t1() {
